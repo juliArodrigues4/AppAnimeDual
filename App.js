6ae444af36fbdc1934ded1e5643bedd0d5840217
julia <134558867+juliArodrigues4@users.noreply.github.com>
2023-10-27 17:08:43 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+import RotasTab from './components/rotastab';
+
+export default function App(){
+  return(
+<NavigationContainer>
+<RotasTab />
+</NavigationContainer>
+  );
+}
+
+
